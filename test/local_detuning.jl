63a4224c7d6c2049e5d49657924eb6b365452f54
Fe-r-oz <ferozahmad7cf@gmail.com>
2024-06-07 00:32:18 +0500

    [Feature]: Full support for local detuning

---
+++ test/local_detuning.jl
@@ -0,0 +1,49 @@
+using Braket, Test, JSON3, StructTypes, Mocking, UUIDs, DecFP
+
+Mocking.activate()
+
+struct MockRydbergLocal
+    timeResolution::Dec128
+    commonDetuningResolution::Dec128
+    localDetuningResolution::Dec128
+end
+
+struct MockRydberg
+    c6coefficient::Dec128
+    rydbergGlobal::Braket.RydbergGlobal
+    rydbergLocal::MockRydbergLocal
+end
+
+struct MockAhsParadigmProperties
+    lattice::Braket.Lattice
+    rydberg::MockRydberg
+end
+
+struct MockAhsDeviceCapabilities
+    action::Dict{Union{Braket.DeviceActionType, String}, Braket.DeviceActionProperties}
+    paradigm::MockAhsParadigmProperties
+end
+
+@testset "AHS.LocalDetuning" begin
+
+	@testset "LocalDetuning" begin
+		times₁ = [0, 0.1, 0.2, 0.3]
+		glob_amplitude₁ = [0.5, 0.8, 0.9, 1.0]
+		pattern₁ = [0.3, 0.7, 0.6, -0.5, 0, 1.6]
+		s₁ = LocalDetuning(times₁, glob_amplitude₁, pattern₁)
+	end
+	
+	@testset "LocalDetuning Stitch: Mean, Left, Right" begin
+		times₁ = [0, 0.1, 0.2, 0.3]
+		glob_amplitude₁ = [0.5, 0.8, 0.9, 1.0]
+		pattern₁ = [0.3, 0.7, 0.6, -0.5, 0, 1.6]	
+		times₂ = [0, 0.1, 0.2, 0.3]
+		glob_amplitude₂ = [0.5, 0.8, 0.9, 1.0]
+		pattern₂ = pattern₁
+		s₂ = LocalDetuning(times₂, glob_amplitude₂, pattern₂)
+		s₁ = LocalDetuning(times₁, glob_amplitude₁, pattern₁)
+		stitchedₗ = stitch(s₁, s₂, :mean)
+		stitchedₗ = stitch(s₁, s₂, :left)
+		stitchedₗ = stitch(s₁, s₂, :right)
+	end
+end
