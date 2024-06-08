9092587c7bdddceee41c3fa5057fcb592cccd990
Fe-r-oz <ferozahmad7cf@gmail.com>
2024-06-08 23:16:43 +0500

    [Feature]: Full support for local detuning - Adding basic tests

--- test/local_detuning.jl
+++ test/local_detuning.jl
@@ -21,25 +21,27 @@ end
 @testset "AHS.LocalDetuning" begin
 
 	@testset "LocalDetuning" begin
-		times₁ = [0, 0.1, 0.2, 0.3]
-		glob_amplitude₁ = [0.5, 0.8, 0.9, 1.0]
-		pattern₁ = [0.3, 0.7, 0.6, -0.5, 0, 1.6]
-		s₁ = LocalDetuning(times₁, glob_amplitude₁, pattern₁)
-           #asserts to test this
+           times₁ = [0, 0.1, 0.2, 0.3]
+           times₂ = [0.3, 0.1, 0.2, 0]
+           glob_amplitude₁ = [0.5, 0.8, 0.9, 1.0]
+           pattern₁ = [0.3, 0.7, 0.6, -0.5, 0, 1.6]
+           s₁ = LocalDetuning(times₁, glob_amplitude₁, pattern₁)
+           s₂ = LocalDetuning(times₂, glob_amplitude₁, pattern₁)
+           @test s₁.magnitude.pattern.series == [0.3, 0.7, 0.6, -0.5, 0, 1.6]
+           @test s₁.magnitude.time_series.sorted == true
+           @test s₂.magnitude.time_series.sorted == false
 	end
 	
 	@testset "LocalDetuning Stitch: Mean, Left, Right" begin
-		times₁ = [0, 0.1, 0.2, 0.3]
-		glob_amplitude₁ = [0.5, 0.8, 0.9, 1.0]
-		pattern₁ = [0.3, 0.7, 0.6, -0.5, 0, 1.6]	
-		times₂ = [0, 0.1, 0.2, 0.3]
-		glob_amplitude₂ = [0.5, 0.8, 0.9, 1.0]
-		pattern₂ = pattern₁
-		s₂ = LocalDetuning(times₂, glob_amplitude₂, pattern₂)
-		s₁ = LocalDetuning(times₁, glob_amplitude₁, pattern₁)
-		stitchedₗ = stitch(s₁, s₂, :mean)
-		stitchedₗ = stitch(s₁, s₂, :left)
-		stitchedₗ = stitch(s₁, s₂, :right)
- 		 #asserts to test this
+           times₁ = [0, 0.1, 0.2, 0.3]
+           glob_amplitude₁ = [0.5, 0.8, 0.9, 1.0]
+           pattern₁ = [0.3, 0.7, 0.6, -0.5, 0, 1.6]	
+           times₂ = [0, 0.1, 0.2, 0.3]
+           glob_amplitude₂ = [0.5, 0.8, 0.9, 1.0]
+           pattern₂ = pattern₁
+           s₂ = LocalDetuning(times₂, glob_amplitude₂, pattern₂)
+           s₁ = LocalDetuning(times₁, glob_amplitude₁, pattern₁)
+           stitchedₗ = stitch(s₁, s₂, :left)
+           @test stitchedₗ.magnitude.pattern == s₁.magnitude.pattern
 	end
 end
