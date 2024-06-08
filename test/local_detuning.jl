e47b78e34c5ea73661b06f2bb01dc5f5459b375d
Fe-r-oz <ferozahmad7cf@gmail.com>
2024-06-08 23:53:42 +0500

    [Feature]: Adding full support for local detuning - Discretize Mock Tests

--- test/local_detuning.jl
+++ test/local_detuning.jl
@@ -44,4 +44,18 @@ end
            stitchedₗ = stitch(s₁, s₂, :left)
            @test stitchedₗ.magnitude.pattern == s₁.magnitude.pattern
 	end
+
+     @testset "LocalDetuning: Discretize" begin
+          times₅ = [0, 0.1, 0.2]
+          values₅ = [0.2, 0.5, 0.7]
+          pattern₅ = [0.1, 0.3, 0.5]
+          ld = LocalDetuning(times₅, values₅, pattern₅)
+          properties = Braket.DiscretizationProperties(
+                 Braket.Lattice(Braket.Area(Dec128("1e-3"), Dec128("1e-3")), Braket.Geometry(Dec128("1e-7"), Dec128("1e-7"), Dec128("1e-7"), 200)),
+                 MockRydberg(Dec128("1e-6"), Braket.RydbergGlobal((Dec128("1.0"), Dec128("1e6")), Dec128("400.0"), Dec128("0.2"), (Dec128("1.0"), Dec128("1e6")), Dec128("0.2"), Dec128("0.2"), (Dec128("1.0"), Dec128("1e6")), Dec128("5e-7"), Dec128("1e-9"), Dec128("1e-5"), Dec128("0.0"), Dec128("100.0")),
+                 MockRydbergLocal(Dec128("1e-9"), Dec128("2000.0"), Dec128("0.01")))
+             )
+          discretizedₗ = discretize(ld, properties)
+          @test discretized_ld isa LocalDetuning
+     end   
 end
